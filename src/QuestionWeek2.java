d4405a0b04fd78041591d8805f1472c281ff1023
Parkjuhyeon1 <202430314@email.daelim.ac.kr>
2024-09-11 17:18:45 +0900

    처음 커밋

---
+++ src/QuestionWeek2.java
@@ -0,0 +1,8 @@
+public class QuestionWeek2 {
+    public static void main(String[] args) {
+       char letter = 'A';
+       int asciiValue = letter;
+       System.out.println("The asciiValue of " + letter + " is " + asciiValue);
+
+    }
+}
